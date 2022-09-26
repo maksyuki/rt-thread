365bf30631844ca36e4d8fdcf0f9f4bcca440d7f
maksyuki <maksyuki@126.com>
2022-09-26 22:09:30 +0800

    feat: add thread switch func impl

--- bsp/qemu-riscv-virt64/applications/main.c
+++ bsp/qemu-riscv-virt64/applications/main.c
@@ -9,13 +9,64 @@
  */
 
 #include <rtthread.h>
-#include <rthw.h>
-#include <stdio.h>
-#include <string.h>
+
+
+#define THREAD_PRIORITY         3
+#define THREAD_STACK_SIZE       512
+#define THREAD_TIMESLICE        5
+
+/* Counter for each thread */
+volatile rt_uint32_t count[2];
+
+/* Threads 1, 2 share an entry, but the entry parameters are different */
+static void thread_entry(void* parameter)
+{
+    rt_uint32_t value;
+
+    value = (rt_uint32_t)parameter;
+    while (1)
+    {
+        rt_kprintf("thread %d is running\n", value);
+        rt_thread_mdelay(1000); // Delay for a while
+    }
+}
+
+static rt_thread_t tid1 = RT_NULL;
+static rt_thread_t tid2 = RT_NULL;
+
+static void hook_of_scheduler(struct rt_thread* from, struct rt_thread* to)
+{
+    rt_kprintf("from: %s -->  to: %s \n", from->name , to->name);
+}
+
+int scheduler_hook(void)
+{
+    /* Set the scheduler hook */
+    rt_scheduler_sethook(hook_of_scheduler);
+
+    /* Create Thread 1 */
+    tid1 = rt_thread_create("thread1",
+                            thread_entry, (void*)1,
+                            THREAD_STACK_SIZE,
+                            THREAD_PRIORITY, THREAD_TIMESLICE);
+    if (tid1 != RT_NULL)
+        rt_thread_startup(tid1);
+
+    /* Create Thread 2 */
+    tid2 = rt_thread_create("thread2",
+                            thread_entry, (void*)2,
+                            THREAD_STACK_SIZE,
+                            THREAD_PRIORITY,THREAD_TIMESLICE - 5);
+    if (tid2 != RT_NULL)
+        rt_thread_startup(tid2);
+    return 0;
+}
+
+
 
 int main(void)
 {
     rt_kprintf("Hello RISC-V!\n");
-
+    scheduler_hook();
     return 0;
 }
